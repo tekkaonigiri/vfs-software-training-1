d44b8b41ef5955f7d2aaa370918cb0a1b6b112ae
WaitHoldMyBeer <kenanblair2@gmail.com>
2025-11-15 02:27:30 -0800

    initial commit

---
+++ src/navigation/src/navigation.cpp
@@ -0,0 +1,7 @@
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/float32.hpp"
+
+/*TODO: Create target location message type
+Create drone node
+Create navigation listener and navigation interface
+*/
