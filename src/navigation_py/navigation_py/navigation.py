19d68495cc5ea9c0c920a9fb43c50de604ac901b
WaitHoldMyBeer <kenanblair2@gmail.com>
2025-11-18 02:38:49 -0800

    Prepared

--- src/navigation_py/navigation_py/navigation.py
+++ src/navigation_py/navigation_py/navigation.py
@@ -4,6 +4,9 @@ from navigation_py.util import update_screen
 from std_msgs.msg import Float64MultiArray
 import numpy as np
 import random
+import curses
+
+screen = curses.initscr()
 
 class DroneNode(Node):
     def __init__(self):
@@ -16,8 +19,10 @@ class DroneNode(Node):
         self.current_pos = np.array([0.0,0.0])
 
         #defines the publishers for these points
-        self.pos_publisher = self.create_publisher(Float64MultiArray, 'currentpos', 10)
-        self.target_publisher = self.create_publisher(Float64MultiArray, 'targetpos', 10)
+        self.pos_publisher = self.create_publisher(Float64MultiArray, 
+            'currentpos', 10)
+        self.target_publisher = self.create_publisher(Float64MultiArray, 
+            'targetpos', 10)
 
         #updates the points every 0.5 seconds
         timer_period = 0.5
@@ -25,15 +30,18 @@ class DroneNode(Node):
 
         #subscribes to the velocity (published by the solution node)
         self.vel_subscription = self.create_subscription(Float64MultiArray,
-            'measuredvel',
+            'drone_vel',
             self.update_position,
             10)
         
         self.success = False
+        self.count = 0
 
     def run_wind(self):
-        self.current_pos[0] = (max(0, round(self.current_pos[0]+random.uniform(-1.99,1.99),2)))
-        self.current_pos[1] = (max(0, round(self.current_pos[1]+random.uniform(-1.99,2.0),2)))
+        self.current_pos[0] = (max(0, round(self.current_pos[0]
+            +random.uniform(-1.99,1.99),2)))
+        self.current_pos[1] = (max(0, round(self.current_pos[1]
+            +random.uniform(-1.99,2.0),2)))
 
     def timer_callback(self):
         self.run_wind()
@@ -47,31 +55,44 @@ class DroneNode(Node):
         #publishes the data
             self.pos_publisher.publish(pos_msg)
             self.target_publisher.publish(targ_msg)
+            update_screen(self.current_pos, self.target_pos, screen)
+        else:
+            curses.nocbreak()
+            curses.echo()
+            curses.endwin()
+            self.get_logger().info("Completed")
 
     def update_position(self, msg):
         # round to prevent impossible to align float precision
         x_vel = round(msg.data[0], 2)
         y_vel = round(msg.data[1], 2)
 
-        if abs(x_vel) > 20 or abs(y_vel) > 20:
-            self.get_logger().info(
-                "Slow down, the propellers can't spin that fast!"
-            )
+        if (x_vel) > 20:
             x_vel = 20
+        if (x_vel) < -20:
+            x_vel = -20
+        if (y_vel > 20):
             y_vel = 20
+        if (y_vel < -20):
+            y_vel = -20
         
         # update current_position
         x_pos = self.current_pos[0]+x_vel
         y_pos = self.current_pos[1]+y_vel
+        x_pos = min(399, max(0, x_pos))
+        y_pos = min(399, max(0, y_pos))
         self.current_pos = np.array([x_pos, y_pos])
 
         #check for equality
         if (round(self.target_pos[0], 2) == round(x_pos,2) and 
             round(self.target_pos[1], 2) == round(y_pos,2)):
-            self.get_logger().info(
-                "Arrived at destination!"
-            )
-        #update_screen(x_pos, y_pos)
+            self.count+=1
+            if (self.count == 4):
+                self.success = True
+        else:
+            self.count = 0
+        update_screen(self.current_pos, self.target_pos, screen)
+        screen.refresh()
         return
     
 def main(args=None):
@@ -83,3 +104,4 @@ def main(args=None):
 
     drone_node.destroy_node()
     rclpy.shutdown()
+    
--- src/navigation_py/navigation_py/util.py
+++ src/navigation_py/navigation_py/util.py
@@ -1,42 +1,41 @@
 import curses
 import numpy as np
+import rclpy
 
 # screen is 400 x 400
-MAX_SCREEN = 400
-TARGET_ROW_PIXEL = -1
-TARGET_COL_PIXEL = -1
-screen = curses.initscr()
+SIMSIZE = 400
+# TARGET_ROW_PIXEL = -1
+# TARGET_COL_PIXEL = -1
 
 # rounding data to utiliy func
 # translate floats into screen size user is looking at
-# be able to take data given in floats and round that to put onto screen for user to see + understand
+# be able to take data given in floats and round that to 
+# put onto screen for user to see + understand
 # loc_data [x, y] --> need to normalize into actual screen index (row, column)
-def get_coords(loc_data: np.ndarray, height, width):
+# def get_coords(loc_data: np.ndarray, height, width):
 
-    # normalize
-    normalize_x = loc_data[0] / MAX_SCREEN
-    normalize_y = loc_data[1] / MAX_SCREEN
-    # round
-    row = int( round( normalize_x * (height - 1) ) )
-    col = int( round( normalize_y * (width - 1) ) )
+#     # normalize
+#     normalize_x = loc_data[0] / MAX_SCREEN
+#     normalize_y = loc_data[1] / MAX_SCREEN
+#     # round
+#     row = int( round( normalize_x * (height - 1) ) )
+#     col = int( round( normalize_y * (width - 1) ) )
 
-    return row, col
+#     return row, col
 
+# def test_scr(screen):
+#     screen.addstr(5,5,"test")
+#     screen.refresh()
 
 def update_screen(drone_loc: np.ndarray, target_loc: np.ndarray, screen):
+
     height, width = screen.getmaxyx()
-    screen.clear()
-
-    global TARGET_ROW_PIXEL, TARGET_COL_PIXEL
-        # so yk to modify the actual variables rather than reading em locally
-
-    # always update drone but only update target if screen is changed
-    # unsure if this is what u wanted??
-    if TARGET_ROW_PIXEL == -1 or TARGET_COL_PIXEL == -1:
-        TARGET_ROW_PIXEL, TARGET_COL_PIXEL = get_coords(target_loc, height, width)
-
-    drone_row, drone_col = get_coords(drone_loc, height, width)
-
+    SCREENSIZE = min([height, width, 400])
+    drone_row = int(SCREENSIZE/SIMSIZE * drone_loc[1])
+    drone_col = int(SCREENSIZE/SIMSIZE * drone_loc[0])
+    target_row = int(SCREENSIZE/SIMSIZE * target_loc[1])
+    target_col = int(SCREENSIZE/SIMSIZE * target_loc[0])
+    screen.erase()
 
     # TODO:
     # figure out how to draw on the screen
@@ -47,30 +46,33 @@ def update_screen(drone_loc: np.ndarray, target_loc: np.ndarray, screen):
         # change screen icon at that point accordingly
         # else print some symbol "-"
     # is i and j row and col??
-    for i in range(height):
-        for j in range(width):
+    for i in range(SCREENSIZE):
+        for j in range(SCREENSIZE):
             if i == drone_row and j == drone_col:
-                screen.addstr(i, j, '+')
+                screen.addstr(i, j, 'H')
+            elif i == target_row and j == target_col:
+                screen.addstr(i,j, '0')
             else:
                 screen.addstr(i, j, '-')
 
-    screen.addstr(0, 0, f"Terminal size: {height}x{width}")
-    screen.addstr(1, 0, f"Drone: Row = {drone_row}, Column = {drone_col}")
-    screen.addstr(1, 0, f"Target: Row = {TARGET_ROW_PIXEL}, Column = {TARGET_COL_PIXEL}")
+    # screen.addstr(0, 0, f"Terminal size: {height}x{width}")
+    # screen.addstr(1, 0, f"Drone: Row = {drone_row}, Column = {drone_col}")
+    # screen.addstr(1, 0, 
+    #     f"Target: Row = {TARGET_ROW_PIXEL}, Column = {TARGET_COL_PIXEL}")
 
     screen.refresh()
 
 
-def main(screen):
-    height, width = screen.getmaxyx()
+# def main(screen):
+#     height, width = screen.getmaxyx()
 
-    screen.addstr(0, 0, f"Screen Height: {height}")
-    screen.addstr(1, 0, f"Screen Width: {width}")
-    screen.refresh()
+#     screen.addstr(0, 0, f"Screen Height: {height}")
+#     screen.addstr(1, 0, f"Screen Width: {width}")
+#     screen.refresh()
 
-    if curses.is_term_resized(height, width):
-        curses.resize(height, width)
-        screen.clear()
+#     if curses.is_term_resized(height, width):
+#         curses.resize(height, width)
+#         screen.clear()
 
     # TODO:
     # some while loop here to cotinue updating the drone
@@ -84,7 +86,8 @@ def main(screen):
             # use screen.getch()
             # wait for user input before exiting
 
-    screen.addstr(0, 0, f"Terminal size: {height}x{width}") # 0,0 coordinates for string
+#     # 0,0 coordinates for string
+#     screen.addstr(0, 0, f"Terminal size: {height}x{width}") 
 
-if __name__ == '__main__':
-    curses.wrapper(main)
+# def run():
+#     curses.wrapper(main)
